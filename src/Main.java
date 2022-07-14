1ce846799daaf6581139c90bcc69a1ae2fcceeb6
Александра Мурашко <germanalexandera@yandex.ru>
2022-07-14 14:08:27 +0300

    Добавьте файлы проекта.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 100; // стоимость билета
+        int mile = 20; // количества рублей для одной бонусной милли
+
+        System.out.println("Количество начисленных миль за билет:");
+        System.out.println((ticket / mile) + " бонусных миллей");
+
+    }
+}
+
